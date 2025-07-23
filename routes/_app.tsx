f8968b4b36fd1fb023c8f5c0f0e628966a5e03c3
decobot <marlio.fernandes@souaurorasaude.com.br>
2025-07-23 19:50:43 +0000

    Publish 2 from staging into origin/main

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -30,12 +30,12 @@ export default defineApp(async (_req, ctx) => {
         {/* Google Tag Manager */}
         <script
           async
-          src="https://www.googletagmanager.com/gtm.js?id=GTM-N32VLN6N"
+          src="https://www.googletagmanager.com/gtm.js?id=GTM-KGKCB98Q"
         >
         </script>
         <script>
           {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date()); gtag('config', 'GTM-N32VLN6N');`}
+          gtag('js', new Date()); gtag('config', 'GTM-KGKCB98Q');`}
         </script>
       </Head>
 
